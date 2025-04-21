5c2a90445fdfec3d154ddf78df4c233df3499394
thorliu6 <595116169@qq.com>
2025-04-21 22:02:24 +0800

    这就可以让学生的数据长时间存储

---
+++ persistence_method.py
@@ -0,0 +1,12 @@
+import json
+
+
+def students_init():
+    file_name='students_profile.json'
+    with open(file_name) as pf:
+        origin_texts=json.load(pf)
+    return origin_texts
+def marks_record(origin_texts):
+    file_name='students_profile.json'
+    with open(file_name,'w') as f:
+        json.dump(origin_texts, f)
